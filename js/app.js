a4651aad8ba4eac1aa5c660b57bd4e8ca86ef52b
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-01-21 23:43:35 -0500

    feeding baby yoda button adds to energy metric bar

--- js/app.js
+++ js/app.js
@@ -1,11 +1,6 @@
 // define the game variables and metrics here 
 
 
-
-
-
-
-
 // When we click on the instructions button, it will show the instructions
 let instructions = document.querySelector("#instructionsBtn")
 function instructionsClick () {
@@ -21,3 +16,15 @@ function instructionsClose () {
     instructionsModule[0].style.display = "none"
 }
 xout.addEventListener("click", instructionsClose)
+
+
+
+//when we click on the "Feed Crickets" button, it will increase the energy for baby yoda
+let feedTomagotchi = document.querySelector("#feed")
+function feedClick () {
+    let energy = document.getElementById("energy")
+    // allow us to access the value of the energy bar
+    console.log(energy.value)
+    energy.value += 5
+}
+feedTomagotchi.addEventListener("click", feedClick)
