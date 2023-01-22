d421a64d13ce82ff0f9056fccf593bca17ffaf06
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-01-22 17:21:34 -0500

    call increase age method from the class

--- js/app.js
+++ js/app.js
@@ -1,71 +1,4 @@
-// define variables that select the buttons we need, our query selectors
-let feedTomagotchi = document.querySelector("#feedBtn")
-let nameTomagotchi = document.querySelector("#name")
-let sleepTomagotchi = document.querySelector("#sleepBtn")
-let playTomagotchi = document.querySelector("#playBtn")
-
-class Tomagotchi {
-    constructor(){
-        this.name = prompt("What would you like to name Baby Yoda?")
-        this.energy = document.getElementById("energy")
-        this.sleep = document.getElementById("sleep")
-        this.happiness = document.getElementById("happiness")
-    }
-
-    // method to name the tomagotchi
-    nameTomagotchi(){
-        nameTomagotchi.innerHTML = this.name
-    }
-
-    // click feed button to increase tomagotchi's energy each time
-    feedClick(){
-        this.energy.value += 5
-        console.log(this.energy.value)
-    }
-
-    // method to allow tomagotchi to sleep 
-    sleepClick () {
-        this.sleep.value += 5
-        // change the background to night time
-        document.body.style.backgroundImage = "url(/images/night-background.gif)"
-    }
-
-    // method to play with tomagotchi 
-    playClick () {
-        this.happiness.value += 5
-    }
-
-
-
-
-
-}
-
-
-
-
-
-const tomagotchiInstance = new Tomagotchi()
-//call the name method from the class to name the tomagotchi
-tomagotchiInstance.nameTomagotchi()
-
-
-//EVENT LISTENERS
-
-// in order to use a method from the class, we need to close over the new instance of the class with a function
-feedTomagotchi.addEventListener("click", function(){
-    tomagotchiInstance.feedClick()})
-
-sleepTomagotchi.addEventListener("click", function(){
-    tomagotchiInstance.sleepClick()})
-
-playTomagotchi.addEventListener("click", function(){
-    tomagotchiInstance.playClick()})
-
-
-// ------------------------------------------------------------------
-
-
+// SECTION 1 Instructions for the Tomagotchi Game 
 // When we click on the instructions button, it will show the instructions
 let instructions = document.querySelector("#instructionsBtn")
 function instructionsClick () {
@@ -84,70 +17,116 @@ xout.addEventListener("click", instructionsClose)
 
 
 
+// SECTION 2 - DEFINE the variables that select the buttons we need, our query selectors
+let feedTomagotchi = document.querySelector("#feedBtn")
+let nameTomagotchi = document.querySelector("#name")
+let sleepTomagotchi = document.querySelector("#sleepBtn")
+let playTomagotchi = document.querySelector("#playBtn")
+//let ageTomagotchi = parseInt(document.querySelector("#ageNum").innerHTML) 
+
+
+
+//SECTION 3 - Create the class that will house our tomagotchi properties and methods
+class Tomagotchi {
+    constructor(){
+        this.name = prompt("What would you like to name Baby Yoda?")
+        this.energy = document.getElementById("energy")
+        this.sleep = document.getElementById("sleep")
+        this.happiness = document.getElementById("happiness")
+        //this.age = parseInt(document.querySelector("#ageNum").innerHTML)
+        this.age = document.querySelector("#ageNum")
+    }
+
+    // method to name the tomagotchi
+    nameTomagotchi(){
+        nameTomagotchi.innerHTML = this.name
+    }
+
+    // click feed button to increase tomagotchi's energy each time
+    feedClick(){
+        this.energy.value += 5
+    }
+
+    // method to allow tomagotchi to sleep 
+    sleepClick () {
+        this.sleep.value += 5
+        // change the background to night time
+        document.body.style.backgroundImage = "url(/images/night-background.gif)"
+    }
+
+    // method to play with tomagotchi 
+    playClick () {
+        this.happiness.value += 5
+    }
+
+    //setter method to increase the age of the tomagotchi every few seconds
+    increaseAge(){
+        this.age.innerHTML = parseInt(this.age.innerHTML) + 1
+        //console.log(this.age.innerHTML)
+        //console.log(parseInt(this.age.innerHTML))
+        //this.age.innerHTML += 1
+        //console.log(parseInt(this.age.innerHTML))
+    }
+
+
+}
+
+
+
+// SECTION 4 - Instatiate the Tomagotchi
+const tomagotchiInstance = new Tomagotchi()
+//call the name method from the class to name the tomagotchi
+tomagotchiInstance.nameTomagotchi()
 
 
 
 
+//SECTION 5 - Event Listeners
+// in order to use a method from the class, we need to close over the new instance of the class with a function
+feedTomagotchi.addEventListener("click", function(){
+    tomagotchiInstance.feedClick()})
 
-//when we click on the "Feed Crickets" button, it will increase the energy for baby yoda
-// let feedTomagotchi = document.querySelector("#feedBtn")
-// let energy = document.getElementById("energy")
-// function feedClick () {
-//     //let energy = document.getElementById("energy")
-//     // allow us to access the value of the energy bar
-//     console.log(energy.value)
-//     energy.value += 5
-// }
-//feedTomagotchi.addEventListener("click", feedClick)
+sleepTomagotchi.addEventListener("click", function(){
+    tomagotchiInstance.sleepClick()})
 
+playTomagotchi.addEventListener("click", function(){
+    tomagotchiInstance.playClick()})
 
 
-//when we click on the "let tomagotchi sleep" button it will increase his sleep level AND
-// change the background image to night time
-// let sleepTomagotchi = document.querySelector("#sleepBtn")
-// let sleep = document.getElementById("sleep")
-// function sleepClick () {
-    
-//     //access the value of the sleep bar 
-//     console.log(sleep.value)
-//     sleep.value += 50
-//     console.log(sleep.value)
-//     // change the background to night time
-//     document.body.style.backgroundImage = "url(/images/night-background.gif)"
-// }
-// sleepTomagotchi.addEventListener("click", sleepClick)
+// SECTION 6 - Calling class methods that dont use event listeners
+
+// Increase age of tomagotchi
+// need to have explicit reference in order to work 
+let interval = setInterval(function(){tomagotchiInstance.increaseAge()}, 3000);
+//let interval = setInterval(function(){tomagotchiInstance.increaseAge()}, 3000);
+    // basically setTimeout has 2 arguments, a) the anymonous function to run(clearInterval) after b)amount of time
+    // when tomagotchi reaches 100 stop increasing the age
+setTimeout(function(){ clearInterval(interval)}, 300000);
+
+
+// ------------------------------------------------------------------
 
 
 
-// when we click "Play with lighsaber" baby yoda's happiness will increase
-// let playTomagotchi = document.querySelector("#playBtn")
-// let happiness = document.getElementById("happiness")
-// function playClick () {
-    
-//     //access the value of the happiness bar 
-//     console.log(happiness.value)
-//     happiness.value += 50
-//     console.log(happiness.value)
-// }
-// playTomagotchi.addEventListener("click", playClick)
+
 
 
 // Every 5 seconds baby yoda's age will increase 
-let ageTomagotchi = parseInt(document.querySelector("#ageNum").innerHTML) 
+//let ageTomagotchi = parseInt(document.querySelector("#ageNum").innerHTML) 
 
 // create a function that will increase the the age every few seconds
- function increaseAge(){
-        ageTomagotchi += 1
-        document.querySelector("#ageNum").innerHTML = ageTomagotchi
-       //call the changeTomagotchi function 
-       changeTomagotchi()
- }
+//  function increaseAge(){
+//         ageTomagotchi += 1
+//         document.querySelector("#ageNum").innerHTML = ageTomagotchi
+//        //call the changeTomagotchi function 
+//        changeTomagotchi()
+//  }
 
 
-let interval = setInterval(increaseAge, 3000);
-// basically setTimeout has 2 arguments, a) the anymonous function to run(clearInterval) after b)amount of time
-// when yoda reaches 100 stop increasing the age
-setTimeout(function(){ clearInterval(interval)}, 300000);
+// let interval = setInterval(increaseAge, 3000);
+// // basically setTimeout has 2 arguments, a) the anymonous function to run(clearInterval) after b)amount of time
+// // when yoda reaches 100 stop increasing the age
+// setTimeout(function(){ clearInterval(interval)}, 300000);
 
 
 // create a function that will change the yoda's appearance when he hits a certain age
