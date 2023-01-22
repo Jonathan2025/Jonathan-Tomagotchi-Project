cfd0ee9c3be29f2a91a4de9e6d3611635f68411c
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-01-21 23:16:53 -0500

    added instructions open and close

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// define the gaem variables and metrics here 
+
+
+
+
+
+
+
+// When we click on the instructions button, it will show the instructions
+let instructions = document.querySelector("#instructionsBtn")
+function instructionsClick () {
+    let instructionsModule = document.getElementsByTagName("aside")
+    instructionsModule[0].style.display = "block"
+}
+instructions.addEventListener("click", instructionsClick)
+
+// when we out of it, it will remove the instructions from the screen 
+let xout = document.querySelector("#closeInstructions")
+function instructionsClose () {
+    let instructionsModule = document.getElementsByTagName("aside")
+    instructionsModule[0].style.display = "none"
+}
+xout.addEventListener("click", instructionsClose)
